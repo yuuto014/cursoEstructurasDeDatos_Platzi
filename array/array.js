59bb4da5269c33b0a46e1f81193ee91e5014d8d6
yuuto <larrota.juancamilo@gmail.com>
2022-05-23 07:57:52 -0500

    Se agregan metodos de eliminacion del primer item y agregar en la primera posicion, como reto de la clase

--- array/array.js
+++ array/array.js
@@ -30,6 +30,30 @@ class MyArray{
         delete this.data[this.length-1];
         this.length--;
     }
+    unshift(item){
+        this.unshiftItem(0);
+        this.data[0] = item;
+        return 0;
+    }
+    unshiftItem(index){
+        this.length++;
+        for(let i = this.length-1 ; i > index; i--){
+            this.data[i] = this.data[i-1]
+        }
+    }
+    slice(){
+        const item = this.data[0]
+        this.shiftIndex(0);
+        return item;
+    }
 }
 
 const myArray = new MyArray();
+
+myArray.push("samuel");
+myArray.push("lucifer");
+myArray.push("pedro");
+myArray.push("Diego");
+myArray.push("Juan");
+myArray.push("Juan");
+myArray.push("pedro");
