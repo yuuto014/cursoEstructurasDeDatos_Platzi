beef55ef39b13d22229aa5791f5858f7a092449e
yuuto <larrota.juancamilo@gmail.com>
2022-05-23 07:11:27 -0500

    creacion de un objeto para silumar un array donde agregamos metodos como get y push, silumando los metodos de un array

---
+++ array/array.js
@@ -0,0 +1,17 @@
+// const array = ["Juan", "Natalia", "Andres","Daniel"];
+class MyArray{
+    constructor(){
+        this.length  = 0;
+        this.data = {};
+    }
+    get(index){
+        return this.data[index];
+    }
+    push(item){
+        this.data[this.length] = item;
+        this.length++;
+        return this.data;
+    }
+}
+
+const myArray = new MyArray();
