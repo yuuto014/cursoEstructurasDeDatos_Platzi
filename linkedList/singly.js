411a0665b2022b6fbe05d1091e5700362ccda5d3
yuuto <larrota.juancamilo@gmail.com>
2022-05-24 15:58:26 -0500

    Uso de Liked list, creamos una clase para representar una singly linked list

---
+++ linkedList/singly.js
@@ -0,0 +1,32 @@
+ // 1 --> 2 --> 3 --> 4 --> 5 --> null
+
+let singlyLinkedList = {
+    value: 1, next:{
+        value: 2, next: {
+            value: 3, next: {
+                value: 4, next: null
+            }
+        }
+    }
+}
+
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class MySinglyLinkedList {
+    constructor(value){
+        this.head = {
+            value: value,
+            next: null,
+        }
+        this.tail = this.head;
+
+        this.length = 1;
+    }
+}
+
+let mySinglyLinkedList = MySinglyLinkedList(1);
