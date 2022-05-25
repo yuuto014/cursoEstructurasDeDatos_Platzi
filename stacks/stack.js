7510bee24359c3118742f1678a0c4fe109aa9899
yuuto <larrota.juancamilo@gmail.com>
2022-05-25 15:10:12 -0500

    implementacion del metodo pop en stack

--- stacks/stack.js
+++ stacks/stack.js
@@ -29,6 +29,30 @@ class Stack{
         
         return this;
     }
+    pop(){
+        if(this.lenght === 0 ){
+            return `No hay datos en el stack`
+        }
+        else if(this.lenght === 1){
+            const holdingPointer = this.top;
+            this.top = null;
+            this.botton = null;
+            this.lenght--;
+            return this;
+        }
+        else{
+            const holdingPointer = this.top;
+            const pointer = this.top.next;
+            this.top = pointer;
+            this.top.next = pointer.next;
+            this.lenght--;     
+            return this;
+        }
+    }
 }
 
 const myStack = new Stack();
+myStack.push(1);
+myStack.push(2);
+myStack.push(3);
+myStack.peek();
